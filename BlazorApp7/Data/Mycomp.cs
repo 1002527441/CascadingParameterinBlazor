92c7f95f5f39d300405b4c78f05dbe8b218f4550
henry.zhang <1002527441@qq.com>
2020-05-27 21:45:40 +0800

    CascadingParameterinBlazor

---
+++ BlazorApp7/Data/Mycomp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Rendering;
+
+namespace BlazorApp7.Data
+{
+    public class Mycomp: ComponentBase
+    {
+        protected override void BuildRenderTree(RenderTreeBuilder builder)
+        {
+            builder.AddMarkupContent(0, "<span> BuildRenderTree  使用 AddMarkupContent 输出 Html 。</span>");
+            //base.BuildRenderTree(builder);//加断点
+         
+        }
+    }
+}
